c71b1bd511fa19d4222cb975b8d676bb450ea176
johnniesimpson <johnniesimpson@outlook.com>
2021-02-07 14:42:33 -0500

    Add case functions to the server. Add list of questions.

--- server.js
+++ server.js
@@ -31,30 +31,77 @@ const start = () => {
         message: 'Test the server',
         choices: [
             'View all employees',
-            'This is a test 2',
-            'This is a test 3',
+            'View all employees by department',
+            'View all employees by manager',
+            'Add employee',
+            'Remove employee',
+            'Update employee role',
+            'Update employee manager',
+            'View all roles',
+            'Add role',
+            'Remove role',
+            'View all departments',
+            'Add department',
+            'Remove department',
+            'View department budget',
+            'Exit',
         ]
     }).then((data) => {
         switch(data.action) {
             case 'View all employees':
                 employeeSearch();
                 break;
-            
-            case 'This is a test 2':
-                console.log('this also works');
+            case 'View all employees by department':
+                employeeDept();
                 break;
-
-            case 'This is a test 3':
-                console.log('You are good to go');
-                break
+            case 'View all employees by manager':
+                employeeManager();
+                break;
+            case 'Add employee':
+                touchEmployee();
+                break;
+            case 'Remove employee':
+                remEmployee();
+                break;
+            case 'Update employee role':
+                updEmployee();
+                break;
+            case 'Update employee manager':
+                updManager();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'Add role':
+                addRole();
+                break;
+            case 'Remove role':
+                remRole();
+                break;
+            case 'View all departments':
+                viewDept();
+                break;
+            case 'Add department':
+                addDept();
+                break;
+            case 'Remove department':
+                remDept();
+                break;
+            case 'View department budget':
+                viewBudget();
+                break;
+            case 'Exit':
+                connection.end();
         }
     })
 };
 
-const employeeSearch = () => {
-    const query = 
-    "SELECT role.department_id, department.name, role.title FROM role JOIN department ON role.department_id = department.id";
-    connection.query(query, (err, res) => {
-        console.table(res)
-    });
-};
+
+
+// const employeeSearch = () => {
+//     const query = 
+//     "SELECT role.department_id, department.name, role.title FROM role JOIN department ON role.department_id = department.id";
+//     connection.query(query, (err, res) => {
+//         console.table(res)
+//     });
+// };
